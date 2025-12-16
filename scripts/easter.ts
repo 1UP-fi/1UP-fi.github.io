1a6396b6716c6ed5532fa45fbd5ee4ef6adfc9f7
1UP-fi <git@1up.fi>
2025-12-16 04:01:12 +0200

    Remove duplicate code

--- scripts/easter.ts
+++ scripts/easter.ts
@@ -16,10 +16,3 @@ fetch("/splashes.txt").then(r=>r.text()).then(txt=>{
     let arr = txt.split('\n').filter(s=>s!='');
     me!.outerHTML = arr[Math.random()*arr.length|0];
 });
-
-
-let me = document.currentScript;
-fetch("/splashes.txt").then(r=>r.text()).then(txt=>{
-    let arr = txt.split('\n').filter(s=>s!='');
-    me!.outerHTML = arr[Math.random()*arr.length|0];
-});
